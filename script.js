9a37012efa7967ee234a0d646879c1e761049dc1
kabir shuvo <kabirshuvo19@gmail.com>
2023-12-26 15:07:22 +0600

    my last commit would be Scanning canvas for pixels rather than text converted into particales

--- script.js
+++ script.js
@@ -116,65 +116,7 @@ window.addEventListener("load", function () {
 
   const effect = new Effect(ctx, canvas.width, canvas.height);
   effect.wrapText("Amraow Tech is the only one");
-//   console.log(effect);
 
   function animate() {}
 
-  //   ctx.lineWidth = 3;
-  //   ctx.strokeStyle = "red";
-  //   ctx.beginPath();
-  //   ctx.moveTo(canvas.width / 2, 0);
-  //   ctx.lineTo(canvas.width / 2, canvas.height);
-  //   ctx.stroke();
-
-  //   ctx.strokeStyle = "green";
-  //   ctx.beginPath();
-  //   ctx.moveTo(0, canvas.height / 2);
-  //   ctx.lineTo(canvas.width, canvas.height / 2);
-  //   ctx.stroke();
-
-  //   const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-  //   gradient.addColorStop(0.3, 'red');
-  //   gradient.addColorStop(0.5, 'orange');
-  //   gradient.addColorStop(0.7, 'fuchsia');
-  //   gradient.addColorStop(1, 'red');
-
-  //   ctx.fillStyle = gradient;
-  //   ctx.strokeStyle = "white";
-  //   ctx.font = "120px Helvitica";
-  //   ctx.textAlign = "center";
-  //   ctx.textBaseline = "middle";
-
-  //   const maxTextWidth = canvas.width * 0.5;
-  //   const lineHeight = 120; //lineHeight should be the same of fontSize.
-
-  //   function wrapText(text) {
-  //     let linesArray = [];
-  //     let lineCounter = 0;
-  //     let line = "";
-  //     let words = text.split(" ");
-  //     for (let i = 0; i < words.length; i++) {
-  //       let testLine = line + words[i] + " ";
-  //       if (ctx.measureText(testLine).width > maxTextWidth) {
-  //         line = words[i] + " ";
-  //         lineCounter++;
-  //       } else {
-  //         line = testLine;
-  //       }
-  //       linesArray[lineCounter] = line;
-  //     }
-  //     let textHeight = lineHeight * lineCounter;
-  //     let textY = canvas.height / 2 - textHeight / 2;
-  //     linesArray.forEach((el, index) => {
-  //       ctx.fillText(el, canvas.width / 2, textY + (index * lineHeight));
-  //     });
-  //     console.log(linesArray);
-  //   }
-
-  // //  wrapText("aaa bbb ccc abc bca cab");
-  //  textInput.addEventListener('keyup', function(e){
-  //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //     wrapText(e.target.value);
-  //     // console.log(e.target.value);
-  //  })
 });
